edee64345e8bfadda4ef3b85756e24271c4d69b2
sdenee01 <speedygonzalezz22@yahoo.com>
2014-02-20 11:11:05 -0500

    added sky and 3 birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -10,8 +10,40 @@ function draw() {
 	var image = document.getElementById("bigImage");
 	if (canvas.getContext) {
 		var ctx = canvas.getContext("2d");
-		ctx.drawImage(image, 0, 273, 77, 123, 50, 260, 144, 246);
-		ctx.drawImage(image, 0, 716, 900, 184, 0, 436+4/9, 800, 163+5/9); 
-		ctx.drawImage(image, 5, 3, 53, 40, 100, 495, 132.5, 100); //Scaled dog 2.5x bigger
+		drawSky(ctx);
+		drawTree(ctx, image);
+		drawDirtAndGrass(ctx, image); 
+		drawDog(ctx, image);
+		drawBirds(ctx, image);
 	}
+}
+
+
+//////////////////////////DRAWING FUNCTIONS////////////////////////////////////
+
+function drawSky(ctx) {
+	ctx.beginPath();
+	ctx.lineTo(600,0);
+	ctx.lineTo(600,800);
+	ctx.lineTo(0,800);
+	ctx.fillStyle = "#87CEEB";
+	ctx.fill();
+}
+
+function drawTree(ctx, image) {
+	ctx.drawImage(image, 0, 273, 77, 123, 50, 260, 144, 246);
+}
+
+function drawDirtAndGrass(ctx, image) {
+	ctx.drawImage(image, 0, 716, 900, 184, 0, 436+4/9, 800, 163+5/9);
+}
+
+function drawDog(ctx, image) {
+	ctx.drawImage(image, 5, 3, 53, 40, 100, 495, 132.5, 100); //Scaled 2.5x
+}
+
+function drawBirds(ctx, image) {
+	ctx.drawImage(image, 0, 120, 34, 25, 100, 100, 68, 50); //Scaled 2x
+	ctx.drawImage(image, 300, 157, 34, 30, 200, 100, 68, 60); //Scaled 2x
+	ctx.drawImage(image, 212, 156, 28, 32, 300, 100, 56, 64); //Scaled 2x
 }
