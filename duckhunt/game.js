4e01511dbf4c3d6cc18438c017e7b87a3500e1dd
sdenee01 <speedygonzalezz22@yahoo.com>
2014-02-20 10:38:35 -0500

    Added tree

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -10,8 +10,8 @@ function draw() {
 	var image = document.getElementById("bigImage");
 	if (canvas.getContext) {
 		var ctx = canvas.getContext("2d");
-		//Scaled dog 2.5x bigger
-		ctx.drawImage(image, 0, 716, 900, 184, 0, 436+4/9, 800, 163+5/9);
-		ctx.drawImage(image, 5, 3, 53, 40, 100, 495, 132.5, 100); 
+		ctx.drawImage(image, 0, 273, 77, 123, 50, 260, 144, 246);
+		ctx.drawImage(image, 0, 716, 900, 184, 0, 436+4/9, 800, 163+5/9); 
+		ctx.drawImage(image, 5, 3, 53, 40, 100, 495, 132.5, 100); //Scaled dog 2.5x bigger
 	}
 }
