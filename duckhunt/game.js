b316c36aa1396244174b055582cc56753fa3178b
sdenee01 <speedygonzalezz22@yahoo.com>
2014-02-19 20:14:21 -0500

    successfully drew dog sniffing ground on the canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,15 @@
-// Your work goes here...
+onload = init;
+
+function init() {
+	draw();
+}
+
+
+function draw() {
+	var canvas = document.getElementById("game");
+	var image = document.getElementById("bigImage");
+	if (canvas.getContext) {
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(image, 5, 3, 53, 40, 400, 300, 53, 40); //x then y
+	}
+}
