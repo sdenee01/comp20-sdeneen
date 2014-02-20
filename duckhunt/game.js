e971fe9453540feffeccb8df7439132f38ed32c1
sdenee01 <speedygonzalezz22@yahoo.com>
2014-02-20 10:23:15 -0500

    drew ground/grass/bushes and placed dog on ground

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -10,6 +10,8 @@ function draw() {
 	var image = document.getElementById("bigImage");
 	if (canvas.getContext) {
 		var ctx = canvas.getContext("2d");
-		ctx.drawImage(image, 5, 3, 53, 40, 400, 300, 53, 40); //x then y
+		//Scaled dog 2.5x bigger
+		ctx.drawImage(image, 0, 716, 900, 184, 0, 436+4/9, 800, 163+5/9);
+		ctx.drawImage(image, 5, 3, 53, 40, 100, 495, 132.5, 100); 
 	}
 }
