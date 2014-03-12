edb2011b3fbc45db8e2ba1ca72e6a9ebb91003e8
sdenee01 <speedygonzalezz22@yahoo.com>
2014-03-12 19:43:39 -0400

    fixed loading bug

--- transit/assignment3.js
+++ transit/assignment3.js
@@ -1,27 +1,24 @@
-lat = -99999;
-lng = -99999;
+google.maps.event.addDomListener(window, 'load', initialize);
 
 function initialize() {
     getMyLocation();
-	var mapOptions = {
-		center: new google.maps.LatLng(lat, lng),
-        zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-    	                          mapOptions);
 }
 
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
 function getMyLocation() {
+        lat = -99999;
+        lng = -99999;
         elem = document.getElementById("loc");
         if (navigator.geolocation) {
             // the navigator.geolocation object is supported on your browser
             console.log("Call before navigator.geolocation");
             navigator.geolocation.getCurrentPosition(function(position) {
                 console.log("Got location");
+                var mapOptions = {
+                    center: new google.maps.LatLng(lat, lng),
+                    zoom: 8
+                };
+                var map = new google.maps.Map(document.getElementById("map-canvas"),
+                                              mapOptions);
                 lat = position.coords.latitude;
                 lng = position.coords.longitude;
                 elem.innerHTML = "<h1>You are in " + lat + ", " + lng + "</h1>";
