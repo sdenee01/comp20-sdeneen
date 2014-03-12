92a6a3855cf5f08282241a16749b56ad0f3f9429
sdenee01 <speedygonzalezz22@yahoo.com>
2014-03-12 16:47:05 -0400

    now prints current lat and long

--- transit/assignment3.js
+++ transit/assignment3.js
@@ -8,4 +8,26 @@ function initialize() {
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+
+onload = function getMyLocation() {
+        lat = -99999;
+        lng = -99999;
+        elem = document.getElementById("loc");
+        if (navigator.geolocation) {
+            // the navigator.geolocation object is supported on your browser
+            console.log("Call before navigator.geolocation");
+            navigator.geolocation.getCurrentPosition(function(position) {
+                console.log("Got location");
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
+                elem.innerHTML = "<h1>You are in " + lat + ", " + lng + "</h1>";
+            });
+            console.log("Made the call to get location");
+            elem.innerHTML = "<h1>You are in " + lat + ", " + lng + "</h1>";
+        }
+        else {
+            alert("Geolocation is not supported by your web browser.  What a shame!");
+        }
+    }
