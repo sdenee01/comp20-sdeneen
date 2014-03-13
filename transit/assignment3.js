5ab4dcf59a2e6a1408e431ec10685a31dd3b6187
sdenee01 <speedygonzalezz22@yahoo.com>
2014-03-12 20:02:17 -0400

    removed console.logs

--- transit/assignment3.js
+++ transit/assignment3.js
@@ -6,9 +6,7 @@ function getMyLocation() {
         elem = document.getElementById("loc");
         if (navigator.geolocation) {
             // the navigator.geolocation object is supported on your browser
-            console.log("Call before navigator.geolocation");
             navigator.geolocation.getCurrentPosition(function(position) {
-                console.log("Got location");
                 lat = position.coords.latitude;
                 lng = position.coords.longitude;
                 var mapOptions = {
@@ -17,10 +15,7 @@ function getMyLocation() {
                 };
                 var map = new google.maps.Map(document.getElementById("map-canvas"),
                                               mapOptions);
-                elem.innerHTML = "<h1>You are in " + lat + ", " + lng + "</h1>";
             });
-            console.log("Made the call to get location");
-            elem.innerHTML = "<h1>You are in " + lat + ", " + lng + "</h1>";
         }
         else {
             alert("Geolocation is not supported by your web browser.  What a shame!");
