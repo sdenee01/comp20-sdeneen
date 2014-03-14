33be3197e1098d754bf1f91935e92e051d2efd62
sdenee01 <speedygonzalezz22@yahoo.com>
2014-03-14 14:16:27 -0400

    bug fixes-

--- transit/assignment3.js
+++ transit/assignment3.js
@@ -90,7 +90,7 @@ function showTStops()
                     content = "<h1>" + markers[m].title + "</h1>";
                     if (data.length > 0) {
                         content = '<table id="schedule"><tr><th>Line</th><th>Trip #</th><th>Direction</th><th>Time Remaining</th></tr>';
-                        for (var j = 0; j < data[i]["schedule"]["Predictions"].length; j++) {
+                        for (var j = 0; j < data[m]["schedule"]["Predictions"].length; j++) {
                             if (data[m].line == color && data[m]["schedule"]["Predictions"][j].Stop == markers[m].title) {
                                 createInfoWindowTable(m, j);
                             }
